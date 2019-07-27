4ebf064e205ff9290949d741e2a39b9d952f4f9b
Clovijan <clovijan@gmail.com>
2019-07-27 11:37:22 -0300

    Conclusão do caso

--- Codigo/Fast Entregas/Model/Cartao.cs
+++ Codigo/Fast Entregas/Model/Cartao.cs
@@ -11,20 +11,22 @@ namespace Model
         public int CodCartao { get; set; }
         [Required]
         [Display(Name ="Número do Cartão")]
+        [StringLength(16)]
         public string Numero { get; set; }
         [Required]
-        [Display(Name ="Nome do Dono")]
+        [Display(Name ="Nome do Titular")]
         [StringLength(50)]
         public string NomeDono { get; set; }
         [Required]
         [Display(Name ="Data de Validade")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString ="{0:dd/mm/yyyy}", ApplyFormatInEditMode =true)]
+        [DisplayFormat(DataFormatString = "{0:dd/mm/yyyy}", ApplyFormatInEditMode = true)]
         public string DataValidade { get; set; }
         [Required]
         [Display(Name ="Código de Segurança")]
         public int Cvv { get; set; }
         [Required]
+        [Display(Name = "CodUsuário")]
         public int CodUsuario { get; set; }
 
     }
