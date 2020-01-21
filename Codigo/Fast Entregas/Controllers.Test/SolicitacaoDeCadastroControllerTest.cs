af4dcea2d4a5deeed75e4af9c69250e1a7de585d
natan <natan.goes777@gmail.com>
2020-01-21 15:56:49 -0300

    Criação das classes de teste

---
+++ Codigo/Fast Entregas/Controllers.Test/SolicitacaoDeCadastroControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+
+namespace Controllers.Test
+{
+    public class SolicitacaoDeCadastroControllerTest
+    {
+        /*[Fact]
+        public void Index_ReturnsAViewResult_WithAListOfEmployees()
+        {
+            // Arrange
+            var mockRepo = new Mock<IDataRepository<Employee>>();
+            mockRepo.Setup(repo => repo.GetAll())
+                .Returns(GetTestEmployees());
+            var controller = new EmployeeController(mockRepo.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<List<Employee>>(
+                viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());
+        }
+
+        private IEnumerable<Employee> GetTestEmployees()
+        {
+            return new List<Employee>()
+            {
+        
+                new Employee()
+                {
+                    Id = 1,
+                    Name = "John"
+                },
+                new Employee()
+                {
+                    Id = 2,
+                    Name = "Doe"
+                }
+            };
+        }*/
+    }
+}
---
+++ Codigo/Fast Entregas/Controllers.Test/UsuarioControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using FastEntregasWeb.Controllers;
+using Services;
+using Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Controllers.Test
+{
+    public class UsuarioControllerTest
+    {
+        
+        [Fact]
+        public void SolicitarCadastroTeste()
+        {
+            // Arrange
+            var mockGerenciadorUsuario = new Mock<IGerenciadorUsuario>();
+            var mockGerenciadorSolicitacao = new Mock<IGerenciadorSolicitacaoDeCadastro>();
+
+            mockGerenciadorSolicitacao.Setup(repo => repo.ObterTodos()).Returns(GetTestSolicitacaoDeCadastro());
+            var controller = new UsuarioController(mockGerenciadorUsuario.Object, mockGerenciadorSolicitacao.Object);
+
+            // Act
+            var usuario = new Usuario();
+            usuario.UserName = "natan";
+            var result = controller.SolicitarCadastro(usuario.UserName);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<List<SolicitacaoDeCadastro>>(
+                viewResult.ViewData.Model);
+            Assert.Single(model);
+        }
+
+        private IEnumerable<SolicitacaoDeCadastro> GetTestSolicitacaoDeCadastro()
+        {
+            return new List<SolicitacaoDeCadastro>()
+            {
+
+                new SolicitacaoDeCadastro()
+                {
+                    CodSolicitacao = 1,
+                    NumRegistro = "123456879123456",
+                    NumCnh = "789456123789456",
+                    DataNascimento = new DateTime(),
+                    CodUsuarioEntregador = 3
+                }
+            };
+        }
+    }
+}
